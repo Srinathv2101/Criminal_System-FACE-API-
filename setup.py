3e490559e00ff2f3f34ec0788d8404975c6e131e
Srinathv2101 <srinathv21@gmail.com>
2022-05-28 23:43:59 +0530

    Key commit

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 config = {
-    'ENDPOINT': 'https://srinathv.cognitiveservices.azure.com/',
-    'KEY': 'd7a163362caa40f5a4f833aa1539f7b4',
+    'ENDPOINT': '{endpoint}',
+    'KEY': '{key}',
     'MAX_REQUEST': 600,
     'REQUEST_TIMEOUT_TIME': 52
 }
